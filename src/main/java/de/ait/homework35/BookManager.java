722355fb2cfd7a41888a64b13f153c8cdaab6e71
AlexeyTschernawski <allexxem@gmail.com>
2023-10-13 22:00:18 +0200

    34: Project created, add pom.xml and Class for test

---
+++ src/main/java/de/ait/homework35/BookManager.java
@@ -0,0 +1,44 @@
+package de.ait.homework35;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookManager {
+
+    private List<Book> books = new ArrayList<>();
+
+    public boolean addBook(Book book) {
+        if (findBookByISBN(book.getIsbn()) != null) {
+            throw new IllegalArgumentException("Book with this ISBN already exists");
+        }
+        books.add(book);
+        return false;
+    }
+
+    public Book findBookByISBN(String isbn) {
+        for (Book book : books) {
+            if (book.getIsbn().equals(isbn)) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    public boolean removeBook(String isbn) {
+        Book book = findBookByISBN(isbn);
+        if (book != null) {
+            return books.remove(book);
+        }
+        return false;
+    }
+
+    public Object findBookByIsbn(String Buch) {
+        return null;
+    }
+
+    public boolean removeBookByIsbn(String Buch) {
+
+        return false;
+    }
+}
+
